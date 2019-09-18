d8456dafa8d71a97ebaa193e0a6da431f7bcf05a
Zoran Dasic <zorandsc1@gmail.com>
2019-09-18 15:58:38 +0200

    1213

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -7,8 +7,7 @@ import menuimg from "../images/bcg/menuBcg.jpeg"
 const MenuPage = () => (
   <Layout>
     <SEO title="Menu" />
-    <div id="fb-root"></div>
-    <div class="fb-customerchat" page_id="119106702808633"></div>
+
     <PageHeader img={menuimg}>
       <Banner title="menu" subtitle={`let's dig in`}></Banner>
     </PageHeader>
