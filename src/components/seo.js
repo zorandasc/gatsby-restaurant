c8d57af97e0b2969ae4f8401391d47ea62d20188
Zoran Dasic <zorandsc@yahoo.com>
2019-09-19 21:28:10 +0200

    676

--- src/components/seo.js
+++ src/components/seo.js
@@ -69,21 +69,6 @@ function SEO({ description, lang, meta, title }) {
         },
       ].concat(meta)}
     >
-      <script>{`window.fbAsyncInit = function() {
-          FB.init({
-            xfbml            : true,
-            version          : 'v4.0'
-          });
-        };
-
-        (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));`}</script>
-
       <script>{`
   window.callbellSettings = {
     token: "3nck19ywCuutTYnHhdAkkfH5"
