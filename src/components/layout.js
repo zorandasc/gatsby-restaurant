3f57c10c607b66e026190a66b14ee14ad39c21d4
Zoran Dasic <zorandsc@yahoo.com>
2019-09-19 21:24:59 +0200

    3232

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,25 +5,13 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React, { useEffect } from "react"
+import React from "react"
 import PropTypes from "prop-types"
 import { createGlobalStyle } from "styled-components"
 import Navbar from "../components/Globals/Navbar/Navbar"
 import Footer from "../components/Globals/Footer"
 
 const Layout = ({ children }) => {
-  useEffect(() => {
-    const div1 = document.createElement("div")
-    div1.id = "fb-root"
-    document.body.appendChild(div1)
-
-    const div2 = document.createElement("div")
-    div2.className = "fb-customerchat"
-    div2.page_id = "119106702808633"
-    div2.setAttribute("page_id", "119106702808633")
-    document.body.appendChild(div2)
-  })
-
   return (
     <main id="main">
       <GlobalStyle />
